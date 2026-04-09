424871093d1e1b71db4ea4b959287cc93b8ea871
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2026-04-09 10:59:07 +0200

    Fix: countdown

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./pages/rules/rules').then(m => m.Rules) },
-    { path: 'play', loadComponent: () => import('./pages/game/game').then(m => m.Game) },
+    { path: 'game', loadComponent: () => import('./pages/game/game').then(m => m.Game) },
     {
         path: '**',
         redirectTo: ''
--- client/src/app/pages/rules/rules.ts
+++ client/src/app/pages/rules/rules.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SharedModule } from '../../../shared.module';
 import { Router } from '@angular/router';
 
@@ -11,7 +11,22 @@ import { Router } from '@angular/router';
 export class Rules {
   private router = inject(Router);
 
-  navigateToGame() {
-    this.router.navigate(['/game']);
+  countdown = signal<number | null>(null);
+
+  startCountdown() {
+    this.countdown.set(3);
+
+    const interval = setInterval(() => {
+      const current = this.countdown();
+      if (current !== null && current > 1) {
+        this.countdown.set(current - 1);
+      } else if (current === 1) {
+        this.countdown.set(0); // Mostra "VIA!"
+      } else {
+        clearInterval(interval);
+        // Navigazione verso il gioco
+        this.router.navigate(['/game']);
+      }
+    }, 1000);
   }
 }
